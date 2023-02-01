459772066f98e77eb6178bfdf37b8d72a46d4bb0
Colin Forster <colin.forster@codafication.com>
2023-02-01 11:16:34 +1000

    testing ts script

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,71 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs'
+import { exec } from 'async-shelljs'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+const isBackend = (n: string) => !(n.endsWith('portal') || n == 'va-customer')
+const compareItems = (a: Item, b: Item) =>  
+  a.TYPE == b.TYPE && a.IMAGE_NAME == b.IMAGE_NAME
+  
+interface Item {
+  TYPE: string  
+  IMAGE_NAME: string  
+  BACKEND: boolean}
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+const run = (folders: string[]) => {
+  // Changed  
+  let changedFiles = exec('git diff --dirstat=files,0 HEAD~1', {
+    silent: true  
+  }).toString()
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
+  let regexp = new RegExp(`(${folders.join('|')})/([\\w-]+)/`, 'g')
+
+  let matches: Item[] = [...changedFiles.matchAll(regexp)].map(x => ({
+    TYPE: x[1],
+    IMAGE_NAME: x[2],
+    BACKEND: isBackend(x[2])
+  }))
+
+  let changed: Item[] = matches.filter(
+    (v, i, a) => i == a.findIndex(x => compareItems(x, v))
+  )
+
+  // Unchanged  
+  let allFolders: Item[] = []
+  for (let base of folders) {
+    let files = fs.readdirSync(base)
+    let mapped: Item[] = files.map(f => ({
+      TYPE: base,
+      IMAGE_NAME: f,
+      BACKEND: isBackend(f)
+    }))
+    allFolders.push(...mapped)
   }
+
+  let unchanged: Item[] = allFolders.filter(
+    a => !changed.find(x => compareItems(a, x))
+  )
+
+  console.log([changed, unchanged])
+
+  return [changed, unchanged]
 }
 
-run()
+run(["src", ".github"])
+
+
+// async function run(): Promise<void> {
+//   try {
+//     const ms: string = core.getInput('milliseconds')
+//     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+
+//     core.debug(new Date().toTimeString())
+//     await wait(parseInt(ms, 10))
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     if (error instanceof Error) core.setFailed(error.message)
+//   }
+// }
+
+// run()
